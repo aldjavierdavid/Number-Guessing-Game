126d253f5be1600792cd8eb30dd7f62ff44f12ae
javier aldana <jdad1701@gmail.com>
2023-05-04 12:21:15 +0200

    terminar

--- app.js
+++ app.js
@@ -3,11 +3,18 @@
 let estoyJugando = true;
 let numeroCorrecto = Math.floor(Math.random() * 100);
 let numIntentos = 10;
+let form = document.querySelector("#guessField")
 
 // Actualizar el innerHTML del nodo adecuado con el número de intentos iniciales
 
 document.querySelector("form").addEventListener("submit", (e) => {
-  e.preventDefault(); // Impedimos que el formulario haga un petición GET
+  e.preventDefault()
+ /*if (numeroCorrecto != valueOf."form" ){
+
+  }; // Impedimos que el formulario haga un petición GET
 
   // Comprobar si el número que ha puesto el usuario es mayor o menor que el numeroCorrecto. Tomar decisiones
-});
+});*/
+;
+console.log(form.value);
+})
